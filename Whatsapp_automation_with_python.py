ad06450c3c4d9fd6a32acfb3af9b4e6901a58096
MeghaSah <74900035+MeghaSah@users.noreply.github.com>
2021-01-03 20:24:32 +0545

    Add files via upload

---
+++ Whatsapp_automation_with_python.py
@@ -0,0 +1,8 @@
+import pywhatkit
+pywhatkit.sendwhatmsg(' +977 1234567890', 'Hi! I have used pywhatkit package.', 19, 57)
+
+#step 1 - In terminal, write command "pip install pywhatkit"
+#   make sure pip is upgraded
+#step 2 - wait to package installed
+#step 3 - once the package is installed , write the above code
+#   give the country code and time accurate
